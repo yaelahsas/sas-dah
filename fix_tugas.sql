a9abee88fa3d2d6c268ea3c3902ecd6880e200bb
yaelahsas <tumbalig001@gmail.com>
2019-06-17 20:56:06 +0700

    Penambahan Sebelum Ready

--- fix_tugas.sql
+++ fix_tugas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2019 at 06:30 PM
+-- Generation Time: May 27, 2019 at 08:03 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -70,6 +70,24 @@ CREATE TABLE `photos` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `photos`
+--
+
+INSERT INTO `photos` (`id`, `file`, `created_at`, `updated_at`) VALUES
+(1, '1557717530sas.jpg', '2019-05-12 20:18:50', '2019-05-12 20:18:50'),
+(2, '1557717801tra.jpg', '2019-05-12 20:23:21', '2019-05-12 20:23:21'),
+(3, '1557717988sas.jpg', '2019-05-12 20:26:28', '2019-05-12 20:26:28'),
+(4, '1557719535sas.jpg', '2019-05-12 20:52:15', '2019-05-12 20:52:15'),
+(5, '1557719612tra.jpg', '2019-05-12 20:53:32', '2019-05-12 20:53:32'),
+(6, '1557720202sas.jpg', '2019-05-12 21:03:22', '2019-05-12 21:03:22'),
+(7, '20190513043552sas.jpg', '2019-05-12 21:35:52', '2019-05-12 21:35:52'),
+(8, '20190513044328FB_IMG_14956384806452422.jpg', '2019-05-12 21:43:28', '2019-05-12 21:43:28'),
+(9, '20190517142113l copy copy.png', '2019-05-17 07:21:13', '2019-05-17 07:21:13'),
+(10, '20190517l copy.jpg', '2019-05-17 08:57:03', '2019-05-17 08:57:03'),
+(11, '201905175055-200.png', '2019-05-17 09:07:26', '2019-05-17 09:07:26'),
+(12, '201905180936055055-200.png', '2019-05-18 02:36:05', '2019-05-18 02:36:05');
+
 -- --------------------------------------------------------
 
 --
@@ -117,8 +135,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `role_id`, `is_active`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `photo_id`) VALUES
-(1, 3, 0, 'inisastra', 'Sastra@ganteng.loh', NULL, 'sadasdads1213', NULL, '2019-05-12 08:00:52', '2019-05-12 08:00:52', NULL),
-(2, 1, 1, 'Dhimas Panji Sastra', 'sastra@ti.poliwangi.ac.id', NULL, 'safasf123', NULL, '2019-05-12 08:01:15', '2019-05-12 08:01:15', NULL);
+(9, 3, 1, 'Ini sound', 'gjfcvsdfgsdfsdfgjf@ujyfifg.k', NULL, '$2y$10$A7fLzct1hgfeA5Vc8KIdJ.gZyh2Q.iHvyJ8QqXrkO0da.K62W3jre', NULL, '2019-05-12 21:35:53', '2019-05-18 02:39:34', '11'),
+(10, 1, 1, 'Dhimas Panji Sastra', 'asfdh@aspkd.jhf', NULL, '$2y$10$37drfyNptQ4orHwUpsKzAeMgMzOCp2WcZ93gXYkYV9hNeiOqCCleq', NULL, '2019-05-12 21:43:28', '2019-05-12 21:43:28', '8'),
+(11, 2, 0, 'kek sas', 'tumbaslig001@gmail.com', NULL, '$2y$10$4unXt1Ki5vZNcWX7CFfrY.zMFayWYb66w09keSiQRazuSXWhw2j2q', NULL, '2019-05-17 07:21:14', '2019-05-17 07:21:14', '9'),
+(12, 3, 1, 'Edita baru dan FOto', 'tumbsalig001@gmail.com', NULL, '$2y$10$j3UreaGhmvpxVmLOASmGu.l4VZ7oUZx/1p3HhsdWNmCJt8o39EmCO', NULL, '2019-05-17 08:09:10', '2019-05-18 02:40:30', '10'),
+(13, 1, 1, 'asdsadasdsadsadsad', 'tumbsadaslig001@gmail.com', NULL, '$2y$10$RPUqSjINyTN3SAKw.9OZQOh4atpEM63px3Tu0lTafciGy/OtXTmBu', NULL, '2019-05-18 02:36:07', '2019-05-18 02:36:07', '12'),
+(15, 1, 1, 'asd', 'Sasstra@ganteng.loh', NULL, '$2y$10$3FiVO0givi0yxOS.Dxs/s.p.DXbVCbGuL7wjJAE00xj2hyVNT8MaK', NULL, '2019-05-18 21:08:52', '2019-05-18 21:08:52', NULL);
 
 --
 -- Indexes for dumped tables
@@ -170,7 +192,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `photos`
 --
 ALTER TABLE `photos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -182,7 +204,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
